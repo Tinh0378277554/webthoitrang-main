4042e0230113c946c8a846ad19a4d341f69e4586
Tinh0378277554 <thuantinh879@gmail.com>
2023-11-13 09:20:20 +0700

    first commit

---
+++ src/main/java/fashion_shop/Controller/HomeServletController.java
@@ -0,0 +1,26 @@
+package fashion_shop.Controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/home")
+public class HomeServletController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/views/home.jsp");
+            requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+    
+}
